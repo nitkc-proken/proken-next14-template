24aea2ac55be0adaa5c12a01ee42ea510a1af6a0
naotiki <contact@naotiki.me>
2024-06-30 18:48:32 +0900

    chore: add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
 			<div className={styles.description}>
 				<p>
 					Get started by editing&nbsp;
